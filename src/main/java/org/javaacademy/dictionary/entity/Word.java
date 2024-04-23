ebbb1820265aee5423e50fa2dbc325017846f926
Alexey Astapov <astapaa@mail.ru>
2024-04-23 12:36:24 +0300

    Проект инит

---
+++ src/main/java/org/javaacademy/dictionary/entity/Word.java
@@ -0,0 +1,16 @@
+package org.javaacademy.dictionary.entity;
+
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+public class Word implements Comparable<Word> {
+    @NonNull
+    private final String word;
+    private String description;
+
+    @Override
+    public int compareTo(Word word) {
+        return this.word.compareTo(word.getWord());
+    }
+}
---
+++ src/main/java/org/javaacademy/dictionary/repository/WordRepository.java
@@ -0,0 +1,11 @@
+package org.javaacademy.dictionary.repository;
+
+import org.javaacademy.dictionary.entity.Word;
+import java.util.TreeSet;
+
+public class WordRepository {
+    TreeSet<Word> words = new TreeSet<>();
+
+
+
+}
