ffb8fa3db0899ab49fffb533970093401ade77a2
Alexey Astapov <astapaa@mail.ru>
2024-04-24 16:45:59 +0300

    Исправлены мелкие ошибки 2

--- src/main/java/org/javaacademy/dictionary/service/exception/WordEmptyException.java
+++ src/main/java/org/javaacademy/dictionary/service/exception/WordEmptyException.java
@@ -2,9 +2,7 @@ package org.javaacademy.dictionary.service.exception;
 
 import lombok.experimental.StandardException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 @StandardException
 @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Не все поля заполнены!")
