481ce37988a183788417f8e224d1d60d10b3143d
Alexey Astapov <astapaa@mail.ru>
2024-04-24 16:44:19 +0300

    Исправлены мелкие ошибки

--- src/main/java/org/javaacademy/dictionary/service/exception/NotComplyFillingRulesException.java
+++ src/main/java/org/javaacademy/dictionary/service/exception/NotComplyFillingRulesException.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @StandardException
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Ошибка заполнения!Слово на английском, описание на русском языке + знаки препинания!")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Ошибка заполнения!Слово на английском,"
+        + " описание на русском языке + знаки препинания!")
 public class NotComplyFillingRulesException extends RuntimeException{
 }
